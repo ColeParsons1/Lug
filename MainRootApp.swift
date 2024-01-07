a6c543aa2f0cbff73378f3cccf04beaed1b726fb
Cole Parsons <47366527+ColeParsons1@users.noreply.github.com>
2024-01-07 14:01:50 -0500

    Add files via upload

---
+++ MainRootApp.swift
@@ -0,0 +1,422 @@
+//
+//  MainRootApp.swift
+//  MainRoot
+//
+//  Created by Cole Parsons on 10/27/22.
+//
+
+import SwiftUI
+import UIKit
+import MapKit
+import Combine
+import Firebase
+import ElegantPages
+import ElegantCalendar
+import Stripe
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+        
+    static var orientationLock = UIInterfaceOrientationMask.all //By default you want all your views to rotate freely
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        let stripeHandled = StripeAPI.handleURLCallback(with: url)
+        if (stripeHandled) {
+            return true
+        } else {
+            // This was not a Stripe url – handle the URL normally as you would
+        }
+        return false
+    }
+
+    // This method handles opening universal link URLs (for example, "https:/example.com/stripe_ios_callback")
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool  {
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+            if let url = userActivity.webpageURL {
+                let stripeHandled = StripeAPI.handleURLCallback(with: url)
+                if (stripeHandled) {
+                    return true
+                } else {
+                    // This was not a Stripe url – handle the URL normally as you would
+                }
+            }
+        }
+        return false
+    }
+}
+
+@main
+struct MainRootApp: App {
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @State var profiles = [Account]();
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
+
+public extension UIDevice {
+
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+
+        func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
+            #if os(iOS)
+            switch identifier {
+            case "iPod5,1":                                       return "iPod touch (5th generation)"
+            case "iPod7,1":                                       return "iPod touch (6th generation)"
+            case "iPod9,1":                                       return "iPod touch (7th generation)"
+            case "iPhone3,1", "iPhone3,2", "iPhone3,3":           return "iPhone 4"
+            case "iPhone4,1":                                     return "iPhone 4s"
+            case "iPhone5,1", "iPhone5,2":                        return "iPhone 5"
+            case "iPhone5,3", "iPhone5,4":                        return "iPhone 5c"
+            case "iPhone6,1", "iPhone6,2":                        return "iPhone 5s"
+            case "iPhone7,2":                                     return "iPhone 6"
+            case "iPhone7,1":                                     return "iPhone 6 Plus"
+            case "iPhone8,1":                                     return "iPhone 6s"
+            case "iPhone8,2":                                     return "iPhone 6s Plus"
+            case "iPhone8,4":                                     return "iPhone SE"
+            case "iPhone9,1", "iPhone9,3":                        return "iPhone 7"
+            case "iPhone9,2", "iPhone9,4":                        return "iPhone 7 Plus"
+            case "iPhone10,1", "iPhone10,4":                      return "iPhone 8"
+            case "iPhone10,2", "iPhone10,5":                      return "iPhone 8 Plus"
+            case "iPhone10,3", "iPhone10,6":                      return "iPhone X"
+            case "iPhone11,2":                                    return "iPhone XS"
+            case "iPhone11,4", "iPhone11,6":                      return "iPhone XS Max"
+            case "iPhone11,8":                                    return "iPhone XR"
+            case "iPhone12,1":                                    return "iPhone 11"
+            case "iPhone12,3":                                    return "iPhone 11 Pro"
+            case "iPhone12,5":                                    return "iPhone 11 Pro Max"
+            case "iPhone12,8":                                    return "iPhone SE (2nd generation)"
+            case "iPhone13,1":                                    return "iPhone 12 mini"
+            case "iPhone13,2":                                    return "iPhone 12"
+            case "iPhone13,3":                                    return "iPhone 12 Pro"
+            case "iPhone13,4":                                    return "iPhone 12 Pro Max"
+            case "iPhone14,4":                                    return "iPhone 13 mini"
+            case "iPhone14,5":                                    return "iPhone 13"
+            case "iPhone14,2":                                    return "iPhone 13 Pro"
+            case "iPhone14,3":                                    return "iPhone 13 Pro Max"
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":      return "iPad 2"
+            case "iPad3,1", "iPad3,2", "iPad3,3":                 return "iPad (3rd generation)"
+            case "iPad3,4", "iPad3,5", "iPad3,6":                 return "iPad (4th generation)"
+            case "iPad6,11", "iPad6,12":                          return "iPad (5th generation)"
+            case "iPad7,5", "iPad7,6":                            return "iPad (6th generation)"
+            case "iPad7,11", "iPad7,12":                          return "iPad (7th generation)"
+            case "iPad11,6", "iPad11,7":                          return "iPad (8th generation)"
+            case "iPad12,1", "iPad12,2":                          return "iPad (9th generation)"
+            case "iPad4,1", "iPad4,2", "iPad4,3":                 return "iPad Air"
+            case "iPad5,3", "iPad5,4":                            return "iPad Air 2"
+            case "iPad11,3", "iPad11,4":                          return "iPad Air (3rd generation)"
+            case "iPad13,1", "iPad13,2":                          return "iPad Air (4th generation)"
+            case "iPad2,5", "iPad2,6", "iPad2,7":                 return "iPad mini"
+            case "iPad4,4", "iPad4,5", "iPad4,6":                 return "iPad mini 2"
+            case "iPad4,7", "iPad4,8", "iPad4,9":                 return "iPad mini 3"
+            case "iPad5,1", "iPad5,2":                            return "iPad mini 4"
+            case "iPad11,1", "iPad11,2":                          return "iPad mini (5th generation)"
+            case "iPad14,1", "iPad14,2":                          return "iPad mini (6th generation)"
+            case "iPad6,3", "iPad6,4":                            return "iPad Pro (9.7-inch)"
+            case "iPad7,3", "iPad7,4":                            return "iPad Pro (10.5-inch)"
+            case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":      return "iPad Pro (11-inch) (1st generation)"
+            case "iPad8,9", "iPad8,10":                           return "iPad Pro (11-inch) (2nd generation)"
+            case "iPad13,4", "iPad13,5", "iPad13,6", "iPad13,7":  return "iPad Pro (11-inch) (3rd generation)"
+            case "iPad6,7", "iPad6,8":                            return "iPad Pro (12.9-inch) (1st generation)"
+            case "iPad7,1", "iPad7,2":                            return "iPad Pro (12.9-inch) (2nd generation)"
+            case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":      return "iPad Pro (12.9-inch) (3rd generation)"
+            case "iPad8,11", "iPad8,12":                          return "iPad Pro (12.9-inch) (4th generation)"
+            case "iPad13,8", "iPad13,9", "iPad13,10", "iPad13,11":return "iPad Pro (12.9-inch) (5th generation)"
+            case "AppleTV5,3":                                    return "Apple TV"
+            case "AppleTV6,2":                                    return "Apple TV 4K"
+            case "AudioAccessory1,1":                             return "HomePod"
+            case "AudioAccessory5,1":                             return "HomePod mini"
+            case "i386", "x86_64", "arm64":                                return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
+            default:                                              return identifier
+            }
+            #elseif os(tvOS)
+            switch identifier {
+            case "AppleTV5,3": return "Apple TV 4"
+            case "AppleTV6,2": return "Apple TV 4K"
+            case "i386", "x86_64": return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "tvOS"))"
+            default: return identifier
+            }
+            #endif
+        }
+
+        return mapToDevice(identifier: identifier)
+    }()
+
+}
+
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+        let darkGray = Color(
+                uiColor: UIColor.darkGray
+        )
+        //let blackPearl = Color("blackPearl")
+    }
+    
+}
+
+
+
+extension View {
+    func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
+        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
+    }
+}
+
+extension Image {
+    func data(url:URL) -> Self {
+        if let data = try? Data(contentsOf: url) {
+            return Image(uiImage: UIImage(data: data)!)
+                .resizable()
+        }
+        return self
+            .resizable()
+    }
+}
+
+extension Int: Identifiable {
+    public var id: Int { self }
+}
+
+extension Date {
+
+    static func daysFromToday(days: Int) -> Date {
+        var date = Date().addingTimeInterval(TimeInterval(60*60*24*days))
+        //getString(d: date)
+        
+        return date
+    }
+    
+    static func getString(d: Date) -> String{
+        let string = "20:32 Wed, 30 Oct 2019"
+        let formatter4 = DateFormatter()
+        formatter4.dateFormat = "HH:mm E, d MMM y"
+        print(formatter4.date(from: string) ?? "Unknown date")
+        
+        return string
+    }
+
+}
+
+extension Date {
+
+    var fullDate: String {
+        DateFormatter.fullDate.string(from: self)
+    }
+
+    var timeOnlyWithPadding: String {
+        DateFormatter.timeOnlyWithPadding.string(from: self)
+    }
+
+}
+
+extension DateFormatter {
+
+    static var fullDate: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMM d, yyyy"
+        return formatter
+    }
+
+    static let timeOnlyWithPadding: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        return formatter
+    }()
+
+}
+
+extension CalendarTheme {
+
+    static let `default`: CalendarTheme = .royalBlue
+
+}
+
+
+struct CalendarThemeKey: EnvironmentKey {
+
+    static let defaultValue: CalendarTheme = .default
+
+}
+
+extension EnvironmentValues {
+
+    var calendarTheme: CalendarTheme {
+        get { self[CalendarThemeKey.self] }
+        set { self[CalendarThemeKey.self] = newValue }
+    }
+
+}
+
+
+
+
+
+func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+    if scrollView.panGestureRecognizer.translation(in: scrollView).y < 0{
+        changeTabBar(hidden: true, animated: true)
+    }
+    else{
+        changeTabBar(hidden: false, animated: true)
+    }
+}
+
+func changeTabBar(hidden:Bool, animated: Bool){
+    if UITabBar.appearance().isHidden == hidden{ return }
+    let frame = UITabBar.appearance().frame
+    let offset = hidden ? frame.size.height : -frame.size.height
+    let duration:TimeInterval = (animated ? 0.5 : 0.0)
+    UITabBar.appearance().isHidden = false
+
+    UIView.animate(withDuration: duration, animations: {
+        UITabBar.appearance().frame = frame.offsetBy(dx: 0, dy: offset)
+    }, completion: { (true) in
+        UITabBar.appearance().isHidden = hidden
+    })
+}
+
+
+
+
+
+//extension URLRequest {
+    //mutating func setBasicAuth(username: String, password: String) {
+        //let encodedAuthInfo = String(format: "%@:%@", username, password)
+            //.data(using: String.Encoding.utf8)!
+            //.base64EncodedString()
+        //addValue("Basic \(encodedAuthInfo)", forHTTPHeaderField: "Authorization")
+    //}
+//}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    let imagePickerController = UIImagePickerController()
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    var videoURL: NSURL?
+    
+    @Binding var selectedImage: UIImage
+    @Binding var type: String
+    @Environment(\.presentationMode) private var presentationMode
+    
+    
+
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        if type == "vid"{
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = sourceType
+            imagePicker.delegate = context.coordinator
+            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary) ?? []
+            imagePicker.mediaTypes = ["public.image","public.movie"]
+            //imagePicker.videoQuality = .typeHigh
+            
+        }
+        
+        if type == "img"{
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = sourceType
+            imagePicker.delegate = context.coordinator
+            imagePicker.mediaTypes = ["public.image","public.movie"]
+        }
+        //imagePicker.videoExportPreset = AVAssetExportPresetHEVC1920x1080
+        
+        return imagePicker
+    }
+    
+    
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            
+            
+            print(parent.selectedImage)
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+struct EdgeBorder: Shape {
+
+    var width: CGFloat
+    var edges: [Edge]
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        for edge in edges {
+            var x: CGFloat {
+                switch edge {
+                case .top, .bottom, .leading: return rect.minX
+                case .trailing: return rect.maxX - width
+                }
+            }
+
+            var y: CGFloat {
+                switch edge {
+                case .top, .leading, .trailing: return rect.minY
+                case .bottom: return rect.maxY - width
+                }
+            }
+
+            var w: CGFloat {
+                switch edge {
+                case .top, .bottom: return rect.width
+                case .leading, .trailing: return self.width
+                }
+            }
+
+            var h: CGFloat {
+                switch edge {
+                case .top, .bottom: return self.width
+                case .leading, .trailing: return rect.height
+                }
+            }
+            path.addPath(Path(CGRect(x: x, y: y, width: w, height: h)))
+        }
+        return path
+    }
+}
+
+
+
+
